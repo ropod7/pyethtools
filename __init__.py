7236b6b6fac6c9872e408b36e339052649030fb6
ropod7 <ropod7@gmail.com>
2016-09-11 23:29:04 +0300

    updates

--- __init__.py
+++ __init__.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 from .request import Request, PersonalRequest, EthConnectionError
-=======
-from .request import Request, PersonalRequest
->>>>>>> e9f291c5bd4cabd6d1453220979ee15d8c334b51
 from . import hextools
 
 __all__ = [Request, PersonalRequest, EthConnectionError, hextools]
